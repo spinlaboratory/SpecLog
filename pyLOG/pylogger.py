68b736874f02c7eff3e95782ab6a2d108469a89c
Yen-Chun.Huang <Yen-Chun.Huang@bruker.com>
2026-04-03 12:44:31 -0400

    Fixed

--- pyLOG/pylogger.py
+++ pyLOG/pylogger.py
@@ -1,5 +1,5 @@
 """
-This is the python program to control pylogger 
+This is the python program to control pyB12logger 
 """
 
 import os
@@ -8,28 +8,32 @@ import argparse
 import shutil
 import subprocess
 from collections import Counter
-from .pyLOG import *
+from .pyB12LOG import *
 
 # auto start and adding icon to desktop (public)
-startup_folder = os.path.normpath(
-    "C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/"
+startup_folder = os.path.join(
+    os.environ["APPDATA"],
+    r"Microsoft\Windows\Start Menu\Programs\Startup"
 )
-desktop_folder = os.path.normpath("C:/Users/Public/Desktop/")
-source_running_logger = (
-    os.path.dirname(sys.executable) + "/scripts/pylogger_running.exe"
-)
-source_monitor = os.path.dirname(sys.executable) + "/scripts/pymonitor.exe"
+desktop_folder = os.path.join(os.environ["USERPROFILE"], "Desktop")
 
+source_running_logger = os.path.join(
+    os.path.dirname(sys.executable), "scripts", "pyB12logger_running.exe"
+)
+
+source_monitor = os.path.join(
+    os.path.dirname(sys.executable), "scripts", "pyB12monitor.exe"
+)
 
 def main_func():
-    parser = argparse.ArgumentParser(prog="pylogger")
+    parser = argparse.ArgumentParser(prog="pyB12logger")
     parser.add_argument(
         "status",
         type=str,
         nargs="?",
         default=None,
         choices=["start", "stop"],
-        help="To start/stop pylogger. If no argument, the pylogger will start by default",
+        help="To start/stop pyB12logger. If no argument, the pyB12logger will start by default",
     )
     parser.add_argument(
         "-desktop",
@@ -43,41 +47,45 @@ def main_func():
         type=str,
         default=None,
         choices=["True", "False"],
-        help="To enable/disable pylogger at startup.",
+        help="To enable/disable pyB12logger at startup.",
     )
     parser.add_argument(
         "-debug",
         type=str,
         default="False",
         choices=["True", "False"],
-        help="To start debug console pylogger.",
+        help="To start debug console pyB12logger.",
     )
     args = parser.parse_args()
 
     if args.startup == "True":
-        if not os.path.exists(startup_folder + "/pylogger_running.exe"):
-            shutil.copy(source_running_logger, startup_folder)
-            print("pylogger will run on startup.")
+        target = os.path.join(startup_folder, "pyB12logger_running.exe")
+        if not os.path.exists(target):
+            shutil.copy(source_running_logger, target)
+            print("pyB12logger will run on startup.")
     elif args.startup == "False":
-        if not os.path.exists(startup_folder + "/pylogger_running.exe"):
-            print(startup_folder + "pylogger_running.exe")
-            print("pylogger does not run on startup.")
+        if not os.path.exists(startup_folder + "/pyB12logger_running.exe"):
+            print(startup_folder + "pyB12logger_running.exe")
+            print("pyB12logger does not run on startup.")
         else:
-            os.remove(startup_folder + "/pylogger_running.exe")
-            print("pylogger will not run on startup.")
+            os.remove(startup_folder + "/pyB12logger_running.exe")
+            print("pyB12logger will not run on startup.")
 
     if args.desktop == "True":
-        if not os.path.exists(desktop_folder + "/pylogger_running.exe"):
-            shutil.copy(source_running_logger, desktop_folder)
-            print("Create pylogger_running.exe on the desktop.")
-        else:
-            print("pylogger_running.exe is on desktop already.")
+        target_logger = os.path.join(desktop_folder, "pyB12logger_running.exe")
+        target_monitor = os.path.join(desktop_folder, "pyB12monitor.exe")
 
-        if not os.path.exists(desktop_folder + "/pymonitor.exe"):
-            shutil.copy(source_monitor, desktop_folder)
-            print("Create pymonitor_running.exe on the desktop.")
+        if not os.path.exists(target_logger):
+            shutil.copy(source_running_logger, target_logger)
+            print("Create pyB12logger_running.exe on the desktop.")
         else:
-            print("pymonitor.exe is on desktop already.")
+            print("pyB12logger_running.exe is on desktop already.")
+
+        if not os.path.exists(target_monitor):
+            shutil.copy(source_monitor, target_monitor)
+            print("Create pyB12monitor.exe on the desktop.")
+        else:
+            print("pyB12monitor.exe is on desktop already.")
 
     if not args.startup and not args.desktop and not args.status:  # not arguments
         args.status = "start"
@@ -93,25 +101,25 @@ def main_func():
         hashDict = Counter(current_exe)
 
         if (
-            "pylogger_running.exe" in hashDict
-            and hashDict["pylogger_running.exe"] > 0
+            "pyB12logger_running.exe" in hashDict
+            and hashDict["pyB12logger_running.exe"] > 0
         ):
-            print("pylogger has started already.")
+            print("pyB12logger has started already.")
             return
 
         else:
             if args.debug == "False":
                 subprocess.Popen(
-                    "pylogger_running.exe", creationflags=subprocess.CREATE_NO_WINDOW
+                    "pyB12logger_running.exe", creationflags=subprocess.CREATE_NO_WINDOW
                 )
-                print("pylogger started")
+                print("pyB12logger started")
             elif args.debug == "True":
-                os.startfile("pylogger_running.exe")
-                print("pylogger debug mode started")
+                os.startfile("pyB12logger_running.exe")
+                print("pyB12logger debug mode started")
 
     elif args.status == "stop":
-        os.system("taskkill /im pylogger_running.exe /F /t")
-        print("pylogger stopped")
+        os.system("taskkill /im pyB12logger_running.exe /F /t")
+        print("pyB12logger stopped")
     else:  # ignore
         return
 
